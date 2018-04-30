846ac8d1c383ebde9d7a6db5b49571df5bc79a76
tutorial88 <38849092+tutorial88@users.noreply.github.com>
2018-04-30 12:04:56 +0700

    Add files via upload

---
+++ GRID/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        AddRow(GRID1)
+    End Sub
+    Sub AddRow(GRID As DataGridView)
+        Dim StringRow As String()
+
+        StringRow = New String() {TextBox1.Text, TextBox2.Text}
+        GRID.Rows.Add(StringRow)
+
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+    End Sub
+End Class
